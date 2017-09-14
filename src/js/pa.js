e4555b4065c224b5eec926cfb8a6481590232c1f
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-14 14:48:38 -0500

    Testing pa.js

--- src/js/pa.js
+++ src/js/pa.js
@@ -1,8 +1,48 @@
 $(function() {
     $(".pa-1").click(function() {
         $(".pa-1").addClass('pa-button-active');
-        for (var j=2; j<17; j++) {
-            $(".pa-" + j ).removeClass('pa-button-active');
+        for (var i=2; i<17; i++) {
+            $(".pa-" + i ).removeClass('pa-button-active');
+        }
+    });
+});
+
+$(function() {
+    $(".pa-2").click(function() {
+        $(".pa-2").addClass('pa-button-active');
+        for (var i=1; i<17; i++) {
+            if (i === 2) { continue; }
+            $(".pa-" + i ).removeClass('pa-button-active');
+        }
+    });
+});
+
+$(function() {
+    $(".pa-3").click(function() {
+        $(".pa-3").addClass('pa-button-active');
+        for (var i=1; i<17; i++) {
+            if (i === 3) { continue; }
+            $(".pa-" + i ).removeClass('pa-button-active');
+        }
+    });
+});
+
+$(function() {
+    $(".pa-4").click(function() {
+        $(".pa-4").addClass('pa-button-active');
+        for (var i=1; i<17; i++) {
+            if (i === 4) { continue; }
+            $(".pa-" + i ).removeClass('pa-button-active');
+        }
+    });
+});
+
+$(function() {
+    $(".pa-5").click(function() {
+        $(".pa-5").addClass('pa-button-active');
+        for (var i=1; i<17; i++) {
+            if (i === 5) { continue; }
+            $(".pa-" + i ).removeClass('pa-button-active');
         }
     });
 });
