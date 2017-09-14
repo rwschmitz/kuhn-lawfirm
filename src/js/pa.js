d78ac5477e660eef0343f91ef5e7f89d7812aee0
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-14 14:40:10 -0500

    Testing pa.js

--- src/js/pa.js
+++ src/js/pa.js
@@ -8,7 +8,7 @@ pa1 = pa2 = pa3 = pa4 = pa5 = pa6 = pa7 = pa8 = pa9 = pa10 = pa11 = pa12 = pa13 
 $(function() {
     $(".pa-1").click(function() {
         $(".pa-1").addClass('pa-button-active');
-        for (i=2; i<17; i++) {
+        for (var i=2; i<17; i++) {
             $(".pa" + i ).removeClass('pa-button-active');
         }
     });
