093f9870e99f56f62eb50009fb8d0c6594bbe656
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-14 14:56:05 -0500

    Testing pa.js

--- src/js/pa.js
+++ src/js/pa.js
@@ -2,7 +2,7 @@ $(function() {
     $(".pa-1").click(function() {
         $(".pa-1").addClass('pa-button-active');
         for (var j=2; j<17; j++) {
-            $(".pa-" + i ).removeClass('pa-button-active');
+            $(".pa-" + j ).removeClass('pa-button-active');
         }
     });
 });
