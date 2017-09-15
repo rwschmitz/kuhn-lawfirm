87c0940f524cadb2bea3907c8720e213805ccaf8
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-14 20:59:58 -0500

    adjusting pa.js

--- src/js/pa.js
+++ src/js/pa.js
@@ -1,205 +1,241 @@
-var si;
-
 $(function() {
-    $(".pa-1").click(function() {
-        $(".pa-1").addClass('pa-button-active');
-        si = 1;
-        for (var i=1; i<19; i++) {
-            if (i === 1) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
-        if (si===1) {
-            $(".subprac-info").text("Hello world!");
-        }
+    var si;
+    
+    $(function() {
+       $(".pa-1").click(function() {
+          $(".pa-1").addClass('pa-button-active');
+          si = 1;
+          for (var i = 1; i < 19; i++) {
+             if (i === 1) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+          if (si === 1) {
+             $(".subprac-info").text("Hello world!");
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-2").click(function() {
-        $(".pa-2").addClass('pa-button-active');
-        si = 2;
-        for (var i=1; i<19; i++) {
-            if (i === 2) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-2").click(function() {
+          $(".pa-2").addClass('pa-button-active');
+          si = 2;
+          for (var i = 1; i < 19; i++) {
+             if (i === 2) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-3").click(function() {
-        $(".pa-3").addClass('pa-button-active');
-        si = 3;
-        for (var i=1; i<19; i++) {
-            if (i === 3) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-3").click(function() {
+          $(".pa-3").addClass('pa-button-active');
+          si = 3;
+          for (var i = 1; i < 19; i++) {
+             if (i === 3) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-4").click(function() {
-        $(".pa-4").addClass('pa-button-active');
-        si = 4;
-        for (var i=1; i<19; i++) {
-            if (i === 4) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-4").click(function() {
+          $(".pa-4").addClass('pa-button-active');
+          si = 4;
+          for (var i = 1; i < 19; i++) {
+             if (i === 4) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-5").click(function() {
-        $(".pa-5").addClass('pa-button-active');
-        si = 5;
-        for (var i=1; i<19; i++) {
-            if (i === 5) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-5").click(function() {
+          $(".pa-5").addClass('pa-button-active');
+          si = 5;
+          for (var i = 1; i < 19; i++) {
+             if (i === 5) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-6").click(function() {
-        $(".pa-6").addClass('pa-button-active');
-        si = 6;
-        for (var i=1; i<19; i++) {
-            if (i === 6) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-6").click(function() {
+          $(".pa-6").addClass('pa-button-active');
+          si = 6;
+          for (var i = 1; i < 19; i++) {
+             if (i === 6) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-7").click(function() {
-        $(".pa-7").addClass('pa-button-active');
-        si = 7;
-        for (var i=1; i<19; i++) {
-            if (i === 7) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-7").click(function() {
+          $(".pa-7").addClass('pa-button-active');
+          si = 7;
+          for (var i = 1; i < 19; i++) {
+             if (i === 7) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-8").click(function() {
-        $(".pa-8").addClass('pa-button-active');
-        si = 8;
-        for (var i=1; i<19; i++) {
-            if (i === 8) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-8").click(function() {
+          $(".pa-8").addClass('pa-button-active');
+          si = 8;
+          for (var i = 1; i < 19; i++) {
+             if (i === 8) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-9").click(function() {
-        $(".pa-9").addClass('pa-button-active');
-        si = 9;
-        for (var i=1; i<19; i++) {
-            if (i === 9) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-9").click(function() {
+          $(".pa-9").addClass('pa-button-active');
+          si = 9;
+          for (var i = 1; i < 19; i++) {
+             if (i === 9) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-10").click(function() {
-        $(".pa-10").addClass('pa-button-active');
-        si = 10;
-        for (var i=1; i<19; i++) {
-            if (i === 10) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-10").click(function() {
+          $(".pa-10").addClass('pa-button-active');
+          si = 10;
+          for (var i = 1; i < 19; i++) {
+             if (i === 10) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-11").click(function() {
-        $(".pa-11").addClass('pa-button-active');
-        si = 11;
-        for (var i=1; i<19; i++) {
-            if (i === 11) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-11").click(function() {
+          $(".pa-11").addClass('pa-button-active');
+          si = 11;
+          for (var i = 1; i < 19; i++) {
+             if (i === 11) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-12").click(function() {
-        $(".pa-12").addClass('pa-button-active');
-        si = 12;
-        for (var i=1; i<19; i++) {
-            if (i === 12) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-12").click(function() {
+          $(".pa-12").addClass('pa-button-active');
+          si = 12;
+          for (var i = 1; i < 19; i++) {
+             if (i === 12) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-13").click(function() {
-        $(".pa-13").addClass('pa-button-active');
-        si = 13;
-        for (var i=1; i<19; i++) {
-            if (i === 13) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-13").click(function() {
+          $(".pa-13").addClass('pa-button-active');
+          si = 13;
+          for (var i = 1; i < 19; i++) {
+             if (i === 13) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-14").click(function() {
-        $(".pa-14").addClass('pa-button-active');
-        si = 14;
-        for (var i=1; i<19; i++) {
-            if (i === 14) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-14").click(function() {
+          $(".pa-14").addClass('pa-button-active');
+          si = 14;
+          for (var i = 1; i < 19; i++) {
+             if (i === 14) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-15").click(function() {
-        $(".pa-15").addClass('pa-button-active');
-        si = 15;
-        for (var i=1; i<19; i++) {
-            if (i === 15) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-15").click(function() {
+          $(".pa-15").addClass('pa-button-active');
+          si = 15;
+          for (var i = 1; i < 19; i++) {
+             if (i === 15) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-16").click(function() {
-        $(".pa-16").addClass('pa-button-active');
-        si = 16;
-        for (var i=1; i<19; i++) {
-            if (i === 16) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-16").click(function() {
+          $(".pa-16").addClass('pa-button-active');
+          si = 16;
+          for (var i = 1; i < 19; i++) {
+             if (i === 16) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-17").click(function() {
-        $(".pa-17").addClass('pa-button-active');
-        si = 17;
-        for (var i=1; i<19; i++) {
-            if (i === 17) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-17").click(function() {
+          $(".pa-17").addClass('pa-button-active');
+          si = 17;
+          for (var i = 1; i < 19; i++) {
+             if (i === 17) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-$(function() {
-    $(".pa-18").click(function() {
-        $(".pa-18").addClass('pa-button-active');
-        si = 18;
-        for (var i=1; i<19; i++) {
-            if (i === 18) { continue; }
-            $(".pa-" + i ).removeClass('pa-button-active');
-        }
+ 
+    $(function() {
+       $(".pa-18").click(function() {
+          $(".pa-18").addClass('pa-button-active');
+          si = 18;
+          for (var i = 1; i < 19; i++) {
+             if (i === 18) {
+                continue;
+             }
+             $(".pa-" + i).removeClass('pa-button-active');
+          }
+       });
     });
-});
-
-
+ });
  
