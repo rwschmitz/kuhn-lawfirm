ef69afbbd6534b8e053a0c2fb0954a29af7fae12
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-14 21:16:27 -0500

    adjusting pa.js

--- src/js/pa.js
+++ src/js/pa.js
@@ -4,7 +4,7 @@ $(function() {
        si = 1;
        for (var i = 1; i < 19; i++) {
           if (i === 1) {
-            $(".subprac-info").fadeIn(1000).text("what's good tho");
+            $(".subprac-info").fadeOut(1000).fadeIn(1000).text("what's good tho");
             continue;
           }
           $(".pa-" + i).removeClass('pa-button-active');
@@ -18,7 +18,7 @@ $(function() {
        si = 2;
        for (var i = 1; i < 19; i++) {
           if (i === 2) {
-            $(".subprac-info").fadeIn(500).text("not much tho");
+            $(".subprac-info").fadeOut(1000).fadeIn(1000).text("not much tho");
             continue;
           }
           $(".pa-" + i).removeClass('pa-button-active');
