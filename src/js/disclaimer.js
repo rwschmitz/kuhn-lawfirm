f6271740dd61de412c09564d041f23277d47af37
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-19 16:05:54 -0500

    Updating disclaimer area

--- src/js/disclaimer.js
+++ src/js/disclaimer.js
@@ -1,20 +1,7 @@
 // This function makes it so that when the disclaimer is clicked, an active class is set.
 
 $(function() {
-    $(".disclaimer-button").on("click", 
-        function() {
-                $(".disclaimer-button").toggleClass('disclaimer-button-active');
-            });
- });
-
- /*
- $(function() {
-    $(".disclaimer-button").on("click",
-        function() {
-            if(active === 1) {
-                $(".disclaimer-button").removeClass('disclaimer-button-active');
-                active = 0;
-            }
+    $(".disclaimer-button").click(function() {
+        $(".disclaimer-button").toggleClass('disclaimer-button-active');
     });
  });
- */
