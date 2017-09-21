9f66ac852b4c0bcc54459c0beb683876e9a3e293
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-21 16:28:44 -0500

    updating thankyou.js

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -1,21 +1,3 @@
-/*window.onload = function thankyou() {
-    console.log("hello world?");
-    var form = document.getElementById("needs-validation");
-    for(var f in form) {
-        form[f].onsubmit = xhr;
-    }
-
-    function xhr() {
-        var endUser = new XMLHttpRequest();
-        endUser.onreadystatechange = displayMessage;
-    }
-    
-    function displayMessage() {
-        if (this.readyState == 4 && this.status == 200) {
-            $("thankYouModal").show();
-        }
-    }
-};*/
 $(function() {
     $('.contact-submit-button').on("click",
     function() {
