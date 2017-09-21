72fb7942a8d7fc84bfb15fc4195de81c87913d10
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-21 17:53:42 -0500

    updating thankyou.js

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -1,47 +1,12 @@
-function contactForm() {
-    $('#needs-validation').submit(function(e) {
-       e.preventDefault();
-       $.ajax({
-          url: 'email_form.php',
-          type: 'post',
-          data: $('#needs-validation').serialize,
-          success: $(function() {
-             $('.contact-submit-button').on("click",
-                function() {
-                   $('#thankYouModal').modal('show');
-                });
-          })
-       });
-    });
- }
-
-
-
-/*function contactForm () {
-    e.preventDefault();
-    $.ajax({
-        url: './src/php/email_form.php',
-        type: 'post',
-        data:$('#needs-validation').serialize,
-        success:function() {
-            $('#thankYouModal').modal('show');
-        }
-    });
-}
-
-
-/*
-$('#needs-valiation').on('submit', function(e) {
-    e.preventDefault();
-    var messageDetails = $('#needs-validation').serialize();
-    $.post('./src/php/email_form.php', messageDetails, function(data) {
-        $('#needs-validation').html(data);
+$(function() {
+    $(".contact-submit-button").click(function() {
+        $.ajax({
+            type: 'post',
+            url: 'email_form.php',
+            data: $('#needs-validation').serialize(),
+            success: function() {
+                ("#thankYouModal").modal('show');
+            }
+        });
     });
 });
-
-$(function() {
-    $('.contact-submit-button').on("click",
-    function() {
-        $('#thankYouModal').modal('show');
-    });
-});*/
