588f0b9dc78165d7d26acecf3e660d9e58fc04ae
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-21 14:48:59 -0500

    testing thankyou.js

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -1,7 +1,7 @@
 window.onload = function thankyou() {
     console.log("hello world?");
     var form = document.getElementById("needs-validation");
-    for(var f in forms) {
+    for(var f in form) {
         frm[f].onsubmit = xhr;
     }
 };
