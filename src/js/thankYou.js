36e450c48dbc77707a68d1eaa224d2881f007575
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-21 16:46:04 -0500

    updating thankyou.js

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -1,3 +1,11 @@
+$('#needs-valiation').on('submit', function(e) {
+    e.preventDefault();
+    var messageDetails = $('#needs-validation').serialize();
+    $.post('./src/php/email_form.php', messageDetails, function(data) {
+        $('#needs-validation').html(data);
+    });
+});
+
 $(function() {
     $('.contact-submit-button').on("click",
     function() {
