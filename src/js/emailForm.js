0bb3de5c9cd8345e3462da02e0d15921df9c9568
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-23 18:52:19 -0500

    updating contact form with new js

--- src/js/emailForm.js
+++ src/js/emailForm.js
@@ -1,13 +1,58 @@
-$(function() {
-  "use strict";
-  window.addEventListener("load", function() {
-    var form = document.getElementById("needs-validation");
-    form.addEventListener("submit", function(event) {
-      if (form.checkValidity() == false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-    }, false);
-  }, false);
-}());
+var form = document.getElementById("needs-validation");
+
+function checkName(e) {
+   var name = document.getElementById("yourName");
+
+   if (name.checkValidity() == false) {
+      alert("name broken");
+      e.preventDefault();
+      e.stopPropagation();
+   } else {
+      return;
+   }
+
+}
+
+function checkEmail(e) {
+   var email = document.getElementById("yourEmail");
+
+   if (email.checkValidity() == false) {
+      alert("email broken");
+      e.preventDefault();
+      e.stopPropagation();
+   } else {
+      return;
+   }
+
+}
+
+function checkNumber(e) {
+   var number = document.getElementById("yourPhoneNumber");
+
+   if (number.checkValidity() == false) {
+      alert("number broken")
+      e.preventDefault();
+      e.stopPropagation();
+   } else {
+      return;
+   }
+
+}
+
+function checkMessage(e) {
+   var message = document.getElementById("yourMessage");
+
+   if (message.checkValidity() == false) {
+      alert("message broken")
+      e.preventDefault();
+      e.stopPropagation();
+   } else {
+      return;
+   }
+
+}
+
+form.addEventListener("submit", checkName, false);
+form.addEventListener("submit", checkEmail, false);
+form.addEventListener("submit", checkNumber, false);
+form.addEventListener("submit", checkMessage, false);
