8b3b39170b179233e2b5c729665d44dd3f7de46f
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-24 16:13:07 -0500

    updating form with jquery

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -4,20 +4,22 @@ var email = document.getElementById("yourEmail");
 var number = document.getElementById("yourPhoneNumber");
 var message = document.getElementById("yourMessage");
 
-$(function() {
-   $('#needs-validation').on('submit', function(e) {
-      if (name.checkValidity() == false || email.checkValidity() == false || number.checkValidity() == false || message.checkValidity() == false) {
-         alert("form broken");
-         e.preventDefault();
-         e.stopPropagation();
-      } else {
-         $.ajax({
-            type: 'POST',
-            url: 'email_form.php',
-            data: $('#needs-validation').serialize(),
-            done: $('#thankYouModal').modal('show'),
-         });
-         return false;
-      }
+$(document).ready(function() {
+   $(function() {
+      $('#needs-validation').on('submit', function(e) {
+         if (name.checkValidity() == false || email.checkValidity() == false || number.checkValidity() == false || message.checkValidity() == false) {
+            alert("form broken");
+            e.preventDefault();
+            e.stopPropagation();
+         } else {
+            $.ajax({
+               type: 'POST',
+               url: 'email_form.php',
+               data: $('#needs-validation').serialize(),
+               done: $('#thankYouModal').modal('show'),
+            });
+            return false;
+         }
+      });
    });
 });
