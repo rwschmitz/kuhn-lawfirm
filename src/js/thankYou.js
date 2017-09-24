8b902258326de89135fba373a99c9ba945cdebb5
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-24 16:23:56 -0500

    updating form with jquery

--- src/js/thankYou.js
+++ src/js/thankYou.js
@@ -1,25 +1,25 @@
-var form = document.getElementById("needs-validation");
-var name = document.getElementById("yourName");
-var email = document.getElementById("yourEmail");
-var number = document.getElementById("yourPhoneNumber");
-var message = document.getElementById("yourMessage");
-
 $(function() {
-   $(function() {
-      $('#needs-validation').on('submit', function(e) {
-         if (name.checkValidity() == false || email.checkValidity() == false || number.checkValidity() == false || message.checkValidity() == false) {
-            alert("form broken");
-            e.preventDefault();
-            e.stopPropagation();
-         } else {
-            $.ajax({
-               type: 'POST',
-               url: 'email_form.php',
-               data: $('#needs-validation').serialize(),
-               done: $('#thankYouModal').modal('show'),
-            });
-            return false;
-         }
-      });
-   });
-});
+    
+       var form = document.getElementById("needs-validation");
+       var name = document.getElementById("yourName");
+       var email = document.getElementById("yourEmail");
+       var number = document.getElementById("yourPhoneNumber");
+       var message = document.getElementById("yourMessage");
+    
+       $('#needs-validation').on('submit', function(e) {
+          if (name.checkValidity() == false || email.checkValidity() == false || number.checkValidity() == false || message.checkValidity() == false) {
+             alert("form broken");
+             e.preventDefault();
+             e.stopPropagation();
+          } else {
+             $.ajax({
+                type: 'POST',
+                url: 'email_form.php',
+                data: $('#needs-validation').serialize(),
+                done: $('#thankYouModal').modal('show'),
+             });
+             return false;
+          }
+       });
+    });
+    
