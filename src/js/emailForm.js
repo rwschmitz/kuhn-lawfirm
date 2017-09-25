9d8da8e2ddf11d9fffd6f310bbe3a23d4f177408
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-25 00:12:22 -0500

    updating contact form

--- src/js/emailForm.js
+++ src/js/emailForm.js
@@ -1,63 +1,46 @@
-/*function checkName(e) {
-   var name = document.getElementById("yourName");
-
-   if (name.checkValidity() == false) {
-      alert("name broken");
-      e.preventDefault();
-      e.stopPropagation();
-   } else {
-   }
-}
-
-function checkEmail(e) {
-   var email = document.getElementById("yourEmail");
-
-   if (email.checkValidity() == false) {
-      alert("email broken");
-      e.preventDefault();
-      e.stopPropagation();
-   } else {
-   }
-
-}
-
-function checkNumber(e) {
-   var number = document.getElementById("yourPhoneNumber");
-
-   if (number.checkValidity() == false) {
-      alert("number broken");
-      e.preventDefault();
-      e.stopPropagation();
-   } else {
-   }
-
-}
-
-function checkMessage(e) {
-   var message = document.getElementById("yourMessage");
-
-   if (message.checkValidity() == false) {
-      alert("message broken");
-      e.preventDefault();
-      e.stopPropagation();
-   } else {
-   }
-
-}
-
-form.addEventListener("submit", checkName, false);
-form.addEventListener("submit", checkEmail, false);
-form.addEventListener("submit", checkNumber, false);
-form.addEventListener("submit", checkMessage, false);
-
 $(function() {
-  $('#needs-validation').on('submit', function(e) {
-     $.ajax({
-        type: 'POST',
-        url: 'email_form.php',
-        data: $('#needs-validation').serialize(),
-        done: $('#thankYouModal').modal('show'),
+  
+     var form = document.getElementById("needs-validation");
+     var name = document.getElementById("yourName");
+     var email = document.getElementById("yourEmail");
+     var number = document.getElementById("yourPhoneNumber");
+     var message = document.getElementById("yourMessage");
+  
+     $('#needs-validation').on('submit', function(e) {
+        if (name.checkValidity() == false || email.checkValidity() == false || number.checkValidity() == false || message.checkValidity() == false) {
+           e.preventDefault();
+           e.stopPropagation();
+           if (name.checkValidity() == false) {
+              $(".invalid-name").css("display", "block");
+           } else {
+              $(".invalid-name").css("display", "none");
+           }
+           if (email.checkValidity() == false) {
+              $(".invalid-email").css("display", "block");
+           } else {
+              $(".invalid-email").css("display", "none");
+           }
+           if (number.checkValidity() == false) {
+              $(".invalid-number").css("display", "block");
+           } else {
+              $(".invalid-number").css("display", "none");
+           }
+           if (message.checkValidity() == false) {
+              $(".invalid-message").css("display", "block");
+           } else {
+              $(".invalid-message").css("display", "none");
+           }
+        } else {
+           $.ajax({
+              type: 'POST',
+              url: 'email_form.php',
+              data: $('#needs-validation').serialize(),
+              done: $('#thankYouModal').modal('show'),
+           });
+           $(".invalid-name, .invalid-email, .invalid-number, .invalid-message").css("display", "none");
+           document.getElementById("needs-validation").reset();
+           return false;
+        }
      });
-     return false;
   });
-});*/
+  
