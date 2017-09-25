2e9336efb2a1e9dce02140c587469981bdc617dd
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-25 14:59:29 -0500

    updating search functionality

--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,34 @@
 $(function() {
-  $(function() {
-     var $context = $(".context");
-     var $form = $("#dog");
-     var $button = $form.find("button[name='search']");
-     var $input = $form.find("input[name='keyword']");
+  var $context = $(".context");
+  var $form = $("#dog");
+  var $button = $form.find("button[name='search']");
+  var $input = $form.find("input[name='keyword']");
 
-     $button.on("click.search", function() {
-
-        // Determine search term
+  $.ajax({
+    type: "POST",
+    url: "index.html",
+    data: $("#keyword").serialize(),
+    done: $("#keyword").keypress(function(e) {
+      if (e.which == 13) {
         var searchTerm = $input.val();
-
         // Remove old highlights and highlight
         // new search term afterwards
         $context.removeHighlight();
         $context.highlight(searchTerm);
-
-     });
-     $button.trigger("click.search");
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    })
   });
+
+  $button.on("click", function() {
+    // Determine search term
+    var searchTerm = $input.val();
+
+    // Remove old highlights and highlight
+    // new search term afterwards
+    $context.removeHighlight();
+    $context.highlight(searchTerm);
+  });
+  $button.trigger("click");
 });
