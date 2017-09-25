58e6f04a34eba93a9f1676348dc0721bca09dbae
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-25 13:28:02 -0500

    adding search plugin and customizing said plugin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 code for the Kuhn lawfirm website.
+
+I'm using the following plugins:
+
+1. jquery-highlight-5.js
+
+that's it!
---
+++ src/js/search.js
@@ -0,0 +1,19 @@
+$(function() {
+    var $context = $(".context");
+    var $form = $("form");
+    var $button = $form.find("button[name='search']");
+    var $input = $form.find("input[name='keyword']");
+  
+    $button.on("click.search", function() {
+  
+      // Determine search term
+      var searchTerm = $input.val();
+  
+      // Remove old highlights and highlight
+      // new search term afterwards
+      $context.removeHighlight();
+      $context.highlight(searchTerm);
+  
+    });
+    $button.trigger("click.search");
+  });
