bcfdd9742eafcc8acfd61b5d87f5c1ba0eb304d7
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-25 00:35:56 -0500

    updating contact form

--- src/js/emailForm.js
+++ src/js/emailForm.js
@@ -1,5 +1,7 @@
 $(function() {
   
+     $("#yourName, #yourEmail, #yourPhoneNumber, #yourMessage").addClass("default-border");
+  
      var form = document.getElementById("needs-validation");
      var name = document.getElementById("yourName");
      var email = document.getElementById("yourEmail");
@@ -12,23 +14,48 @@ $(function() {
            e.stopPropagation();
            if (name.checkValidity() == false) {
               $(".invalid-name").css("display", "block");
+              $("#yourName").removeClass("default-border");
+              $("#yourName").removeClass("valid-border");
+              $("#yourName").addClass("invalid-border");
            } else {
               $(".invalid-name").css("display", "none");
+              $("#yourName").removeClass("default-border");
+              $("#yourName").removeClass("invalid-border");
+              $("#yourName").addClass("valid-border");
            }
            if (email.checkValidity() == false) {
               $(".invalid-email").css("display", "block");
+              $("#yourEmail").removeClass("default-border");
+              $("#yourEmail").removeClass("valid-border");
+              $("#yourEmail").addClass("invalid-border");
            } else {
               $(".invalid-email").css("display", "none");
+              $("#yourEmail").removeClass("default-border");
+              $("#yourEmail").removeClass("invalid-border");
+              $("#yourEmail").addClass("valid-border");
+  
            }
            if (number.checkValidity() == false) {
               $(".invalid-number").css("display", "block");
+              $("#yourPhoneNumber").removeClass("default-border");
+              $("#yourPhoneNumber").removeClass("valid-border");
+              $("#yourPhoneNumber").addClass("invalid-border");
            } else {
               $(".invalid-number").css("display", "none");
+              $("#yourPhoneNumber").removeClass("default-border");
+              $("#yourPhoneNumber").removeClass("invalid-border");
+              $("#yourPhoneNumber").addClass("valid-border");
            }
            if (message.checkValidity() == false) {
               $(".invalid-message").css("display", "block");
+              $("#yourMessage").removeClass("default-border");
+              $("#yourMessage").removeClass("valid-border");
+              $("#yourMessage").addClass("invalid-border");
            } else {
               $(".invalid-message").css("display", "none");
+              $("#yourMessage").removeClass("default-border");
+              $("#yourMessage").removeClass("invalid-border");
+              $("#yourMessage").addClass("valid-border");
            }
         } else {
            $.ajax({
@@ -38,6 +65,9 @@ $(function() {
               done: $('#thankYouModal').modal('show'),
            });
            $(".invalid-name, .invalid-email, .invalid-number, .invalid-message").css("display", "none");
+           $("#yourName, #yourEmail, #yourPhoneNumber, #yourMessage").removeClass("invalid-border");
+           $("#yourName, #yourEmail, #yourPhoneNumber, #yourMessage").removeClass("valid-border");
+           $("#yourName, #yourEmail, #yourPhoneNumber, #yourMessage").addClass("default-border");
            document.getElementById("needs-validation").reset();
            return false;
         }
