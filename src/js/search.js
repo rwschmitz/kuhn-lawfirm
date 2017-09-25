3e8e098f6bd7bb80e7710238baa2fc6e38937a53
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-25 13:58:47 -0500

    troubleshooting search functionality

--- src/js/search.js
+++ src/js/search.js
@@ -1,19 +1,19 @@
 $(function() {
-    var $context = $(".context");
-    var $form = $("#dog");
-    var $button = $form.find("button[name='search']");
-    var $input = $form.find("input[name='keyword']");
-  
-    $button.on("click.search", function() {
-  
-      // Determine search term
-      var searchTerm = $input.val();
-  
-      // Remove old highlights and highlight
-      // new search term afterwards
-      $context.removeHighlight();
-      $context.highlight(searchTerm);
-  
-    });
-    $button.trigger("click.search");
+  var $context = $(".context");
+  var $form = $("#dog");
+  var $button = $form.find("button[name='search']");
+  var $input = $form.find("input[name='keyword']");
+
+  $button.on("click.search", function() {
+
+    // Determine search term
+    var searchTerm = $input.val();
+
+    // Remove old highlights and highlight
+    // new search term afterwards
+    $context.removeHighlight();
+    $context.highlight(searchTerm);
+
   });
+  $button.trigger("click.search");
+});
